a7a4c3827b2496a259060fcd78c9196117781ecf
user1 <dekster174@inbox.ru>
2020-05-08 14:08:29 +0500

    Сделан brif, изменена шапка

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
+/* Модальное окно */
 var button = document.querySelector('#button');
 var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
@@ -12,5 +13,40 @@ $(document).ready(function(){
         modal.classList.remove('modal_active');
     });
 
-    
+    /* Стрелка прокрутки вниз */
+    $(function(){
+    	$('#scroll_bottom').click(function(){
+    		$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 600);
+    		return false;
+    	});
+    });
+    /* Стрелка прокрутки вверх */
+    $(function(){
+    	$('#scroll_top').click(function(){
+    		$('html, body').animate({scrollTop: 0}, 600);
+    		return false;
+    	});
+    });
+
+    $(function(){
+        $('#scroll_top').hide();
+        $(window).scroll(function(){
+            if($(window).scrollTop() > 200) {
+                $('#scroll_top').show();
+            } else {
+                $('#scroll_top').hide();
+            }
+        });
+    });
+///////////////////////////???
+    $(function(){
+        $(window).scroll(function(){
+            if ($(window).scrollTop() > document.body.scrollHeight - 100) {
+                $('#scroll_bottom').hide();
+            } else {
+                $('#scroll_bottom').show();
+            }
+        });
+    });
+//////////////////////////???
 });
